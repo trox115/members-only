bcded43b1665e948d36924d68852682b71bf7816
trox115 <tofernandes67@gmail.com>
2019-08-27 18:21:59 +0100

    Error from routes gone

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :session, only: [:new, :create, :destroy]
-
+  resources :sessions, only: [:new, :create, :destroy]
   match '/signin',  to: 'sessions#new',         via: 'get'
-match '/signout', to: 'sessions#destroy',     via: 'delete'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
 
 end
